3cd5d5393ec63ed984f68bc8f3fed646b7ed28fb
miftanurfarid <miftanurfarid@gmail.com>
2016-04-17 12:57:22 +0700

    menampilkan dB sinyal vibrasi

--- tugas2-spektrum_target_noise/vibrasi.m
+++ tugas2-spektrum_target_noise/vibrasi.m
@@ -1,18 +1,15 @@
-% tugas2.m
+% vibrasi.m
 %
 % Performansi spektrogram menampilkan spektrum data getaran pompa terhadap
 % kenaikan tingkat tekanan bunyi dari noise (white noise, pink noise, brown
 % noise dan blue noise)
 %
-% 17/04/2016 07:42 PM
+% 17/04/2016 11:27 PM
 % mifta nur farid, miftanurfarid@gmail.com
 % https://github.com/miftanurfarid/
 %
 % Teknik Fisika - Institut Teknologi Sepuluh Nopember
 %__________________________________________________________________________
-close all;
-clear all;
-clc;
 
 load p1-unbalance.lvm          
 
@@ -32,64 +29,8 @@ data.hor  = (data.hor - min(data.hor)).*(xmax - xmin)/(max(data.hor)...
 data.vert = (data.vert - min(data.vert)).*(xmax - xmin)/(max(data.vert)...
     - min(data.vert)) + xmin;
 
-data.fs = 25600;     % frekuensi sampling 25600 Hz
-
-
-
-NFFT = 2^nextpow2(length(data.axi));
+% frekuensi sampling 25600 Hz
+data.fs = 25600;
 
 data.db_axi = mean(db(data.axi));
 fprintf('rata-rata level pada sumbu axial = %.2f dB\n', data.db_axi);
-
-% spektrum axial
-fig = 1;
-figure(fig); fig = fig + 1;
-subplot(131);
-myspectrogram(data.axi, data.fs, [20 5], @hamming, NFFT); axis tight;
-title ('Spektrum Getaran Pompa (Aksial)', 'fontweight','bold',...
-    'fontsize',12);
-xlabel ('waktu (detik)','fontweight','bold','fontsize',12);
-ylabel ('frekuensi (Hz)','fontweight','bold','fontsize',12);
-
-subplot(132);
-myspectrogram(data.hor, data.fs, [20 5], @hamming, NFFT); axis tight;
-title ('Spektrum Getaran Pompa (Horizontal)', 'fontweight','bold',...
-    'fontsize',12);
-xlabel ('waktu (detik)','fontweight','bold','fontsize',12);
-ylabel ('frekuensi (Hz)','fontweight','bold','fontsize',12);
-
-subplot(133);
-myspectrogram(data.hor, data.fs, [20 5], @hamming, NFFT); axis tight;
-title ('Spektrum Getaran Pompa (Vertikal)', 'fontweight','bold',...
-    'fontsize',12);
-xlabel ('waktu (detik)','fontweight','bold','fontsize',12);
-ylabel ('frekuensi (Hz)','fontweight','bold','fontsize',12);
-
-% compare signal with signal + white noise
-
-noise.white = randn(length(data.axi),1);
-noise.db_white = mean(db(noise.white));
-fprintf('rata-rata level pada sumbu axial = %.2f dB\n', noise.db_white);
-
-pow = -(noise.db_white - data.db_axi);
-
-noise.white = noise.white*10^(pow/20);
-
-noise.db_white = mean(db(noise.white));
-fprintf('rata-rata level pada sumbu axial = %.2f dB\n', noise.db_white);
-
-data.axi_whi = data.axi + noise.white;
-
-figure(fig); fig = fig + 1;
-subplot(211);
-myspectrogram(data.axi_whi, data.fs, [20 5], @hamming, NFFT); axis tight;
-title ('Spektrum Getaran Pompa (Aksial, White Noise (dB sama))',...
-    'fontweight','bold', 'fontsize', 12);
-xlabel ('waktu (detik)','fontweight','bold','fontsize',12);
-ylabel ('frekuensi (Hz)','fontweight','bold','fontsize',12);
-subplot(212);
-myspectrogram(data.axi, data.fs, [20 5], @hamming, 1024); axis tight;
-title ('Spektrum Getaran Pompa (Aksial)', 'fontweight','bold',...
-    'fontsize', 12);
-xlabel ('waktu (detik)','fontweight','bold','fontsize',12);
-ylabel ('frekuensi (Hz)','fontweight','bold','fontsize',12);
