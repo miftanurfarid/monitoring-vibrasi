e0d67bd900632723ad3049e1652cb1456a8ac73c
miftanurfarid <miftanurfarid@gmail.com>
2016-05-23 12:41:59 +0700

    add description

--- tugas2-spektrum_target_noise/tambah_noise.m
+++ tugas2-spektrum_target_noise/tambah_noise.m
@@ -1,9 +1,13 @@
-function [mix] = tambah_noise(data, pow, type, length)
+function [mix] = tambah_noise(data, pow, type)
 % tambah_noise.m
 %
 % Menambahkan berbagai jenis noise terhadap data getaran pompa dengan
 % variasi SNR.
 %
+% data : data pengukuran vibrasi
+% pow  : power noise
+% type : tipe noise (white, blue, pink, red, violet)
+% 
 % 17/04/2016 11:27 PM
 % mifta nur farid, miftanurfarid@gmail.com
 % https://github.com/miftanurfarid/
@@ -11,48 +15,21 @@ function [mix] = tambah_noise(data, pow, type, length)
 % Teknik Fisika - Institut Teknologi Sepuluh Nopember
 %__________________________________________________________________________
 switch type     % Jenis - jenis noise
-    case 'White' % White Noise
-        noise = randn(length,1);
-        % normalisasi skala -1 hingga 1
-        xmax = 1;
-        xmin = -1;
-        noise  = (noise - min(noise)).*(xmax - xmin)/...
-            (max(noise) - min(noise)) + xmin;
+    case 'White'
+	noise = audioread('white_noise.wav');
         
-    case 'Blue'% Blue Noise
-        noise = bluenoise(length);
-        % normalisasi skala -1 hingga 1
-        xmax = 1;
-        xmin = -1;
-        noise  = (noise - min(noise)).*(xmax - xmin)/...
-            (max(noise) - min(noise)) + xmin;
-        noise = noise';
-        
-    case 'Pink'% Pink Noise
-        noise = pinknoise(length);
-        % normalisasi skala -1 hingga 1
-        xmax = 1;
-        xmin = -1;
-        noise  = (noise - min(noise)).*(xmax - xmin)/...
-            (max(noise) - min(noise)) + xmin;
-        noise = noise';
-        
+    case 'Blue'
+        noise = audioread('blue_noise.wav');
+
+    case 'Pink'
+        noise = audioread('pink_noise.wav');
+
     case 'Red'
-        noise = rednoise(length);
-        % normalisasi skala -1 hingga 1
-        xmax = 1;
-        xmin = -1;
-        noise  = (noise - min(noise)).*(xmax - xmin)/...
-            (max(noise) - min(noise)) + xmin;
-        noise = noise';
+	noise = audioread('red_noise.wav');
+
     case 'Violet'
-        noise = rednoise(length);
-        % normalisasi skala -1 hingga 1
-        xmax = 1;
-        xmin = -1;
-        noise  = (noise - min(noise)).*(xmax - xmin)/...
-            (max(noise) - min(noise)) + xmin;
-        noise = noise';
+	noise = audioread('violet_noise.wav');
+
 end
 
 noise = noise*10^(pow/20);
