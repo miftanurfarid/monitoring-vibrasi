6bcfb52ec2ca5422712de0b4b238e56f39ed083c
miftanurfarid <miftanurfarid@gmail.com>
2016-05-23 12:37:05 +0700

    add display plot vibration

--- tugas2-spektrum_target_noise/vibrasi.m
+++ tugas2-spektrum_target_noise/vibrasi.m
@@ -1,4 +1,4 @@
-function vibrasi(kerusakan, sumbu, jenis_noise, pow)
+function vibrasi(kerusakan, sumbu, jenis_noise, pow, plot)
 % vibrasi.m
 %
 % Performansi spektrogram menampilkan spektrum data getaran pompa terhadap
@@ -8,13 +8,16 @@ function vibrasi(kerusakan, sumbu, jenis_noise, pow)
 % kerusakan:
 % 1 = normal, 2 = unbalance, 3 = novibrasi
 %
-% sumbu:
-% 1 = axial, 2 = horizontal, 3 = vertical
-% 
 % jenis_noise:
 % 'White', 'Blue', 'Pink', 'Red', 'Violet'
 %
+% sumbu:
+% 1 = axial, 2 = horizontal, 3 = vertical
+% 
 % pow = power noise in dB
+%
+% plot:
+% 1 = plot data vibrasi 3 sumbu, 0 = tidak
 %
 % 17/04/2016 11:27 PM
 % mifta nur farid, miftanurfarid@gmail.com
@@ -26,7 +29,7 @@ data.fs = 25600;            % frekuensi sampling 25600 Hz
 data.kerusakan = kerusakan; % jenis kerusakan pompa
 data.Tw = 60;               % windows 50 ms
 data.Ts = 5;                % windows shift 5 ms
-data.window = @hamming;
+data.window = @hamming;     % hamming windows
 data.nfft = 1024;
 
 % jenis kerusakan pompa
@@ -45,10 +48,12 @@ end
 
 fig = 1;
 
-plot_vibrasi(data.vibrasi, data.fs, data.Tw, data.Ts, data.window,...
-    data.nfft, data.kerusakan, fig);
-fig = fig + 1;
+if plot == 1
+    plot_vibrasi(data.vibrasi, data.fs, data.Tw, data.Ts, data.window, data.nfft, data.kerusakan, fig);
+    fig = fig + 1;
+end
 
+% data pengukuran pada sumbu yang akan digunakan
 switch sumbu
     case 1  %axial
         data.type = 'Axial';
@@ -59,9 +64,7 @@ switch sumbu
         data.xmax = 1;
         data.xmin = -1;
         
-        data.vibrasi  = (data.vibrasi - min(data.vibrasi)).*...
-            (data.xmax - data.xmin)/(max(data.vibrasi)...
-            - min(data.vibrasi)) + data.xmin;
+        data.vibrasi  = (data.vibrasi - min(data.vibrasi)).*(data.xmax - data.xmin)/(max(data.vibrasi) - min(data.vibrasi)) + data.xmin;
         
     case 2  %horizontal
         data.type = 'Horizontal';
@@ -72,9 +75,7 @@ switch sumbu
         data.xmax = 1;
         data.xmin = -1;
         
-        data.vibrasi  = (data.vibrasi - min(data.vibrasi)).*...
-            (data.xmax - data.xmin)/(max(data.vibrasi)...
-            - min(data.vibrasi)) + data.xmin;
+        data.vibrasi  = (data.vibrasi - min(data.vibrasi)).*(data.xmax - data.xmin)/(max(data.vibrasi) - min(data.vibrasi)) + data.xmin;
 
     case 3  %vertikal
         data.type = 'Vertikal';
@@ -85,20 +86,16 @@ switch sumbu
         data.xmax = 1;
         data.xmin = -1;
         
-        data.vibrasi  = (data.vibrasi - min(data.vibrasi)).*...
-            (data.xmax - data.xmin)/(max(data.vibrasi)...
-            - min(data.vibrasi)) + data.xmin;
+        data.vibrasi  = (data.vibrasi - min(data.vibrasi)).*(data.xmax - data.xmin)/(max(data.vibrasi) - min(data.vibrasi)) + data.xmin;
 end
 
-data.title = sprintf('Spektrum Getaran Pompa %s ( %s )',...
-    data.kerusakan, data.type);
+data.title = sprintf('Spektrum Getaran Pompa %s ( %s )', data.kerusakan, data.type);
 
 %% tambah noise
 
 noise.pow = pow;
 noise.type = jenis_noise;   % jenis noise
-[data.mix] = tambah_noise(data.vibrasi,...
-    noise.pow, noise.type, length(data.vibrasi));
+[data.mix] = tambah_noise(data.vibrasi, noise.pow, noise.type);
 noise.type = sprintf('%s Noise', jenis_noise);
 
 %% Print SNR
@@ -107,11 +104,7 @@ overall_snr = snr(data.vibrasi, data.mix - data.vibrasi);
 
 %% plot spektrum vibrasi & vibrasi + noise
 
-noise.title = sprintf('Spektrum Getaran Pompa %s ( %s, %.1f dB SNR %s )',...
-    data.kerusakan, data.type, overall_snr, noise.type);
+noise.title = sprintf('Spektrum Getaran Pompa %s ( %s, %.1f dB SNR %s )', data.kerusakan, data.type, overall_snr, noise.type);
 
-plot_vibrasinoise(data.mix, data.vibrasi, data.fs, data.nfft,...
-    noise.title, data.title, data.Tw, data.Ts, fig);
-fig = fig + 1;
-
+plot_vibrasinoise(data.mix, data.vibrasi, data.fs, data.nfft, noise.title, data.title, data.Tw, data.Ts, fig);
 end
