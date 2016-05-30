e3e563a512398895b5246e6ccc98ccd99ca17228
miftanurfarid <miftanurfarid@gmail.com>
2016-05-30 21:10:24 +0700

    plot hasil fft

---
+++ tugas2-spektrum_target_noise/fft_vibrasi.m
@@ -0,0 +1,14 @@
+function fft_vibrasi(y, Fs)
+    L = length(y); % Length of signal
+    NFFT = 2^nextpow2(L); % Next power of 2 from length of y
+    Y = fft(y,NFFT)/L;
+    f = Fs/2*linspace(0,1,NFFT/2+1);
+
+    % Plot single-sided amplitude spectrum.
+    figure;
+    plot(f,2*abs(Y(1:NFFT/2+1))) 
+    title('Single-Sided Amplitude Spectrum of y(t)')
+    xlabel('Frequency (Hz)')
+    ylabel('|Y(f)|')
+    axis tight;
+end
